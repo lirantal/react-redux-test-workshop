90c6bc02a87784f945594eb238614fa338e6db19
marianna <marianna.etelson@nielsen.com>
2018-04-07 07:05:22 +0300

    test(React Components): add unitest for GifItem component

--- src/gif-search-app/components/gifList/GifItem.js
+++ src/gif-search-app/components/gifList/GifItem.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class GifItem extends Component {
 
+  static propTypes = {
+    gif: PropTypes.object.isRequired,
+    onGifSelect: PropTypes.func
+  }
+
+  selectGif = () => {
+    const {onGifSelect, gif} = this.props
+    if (onGifSelect) {
+      onGifSelect(gif)
+    }
+  }
 
   render() {
 
-    const {onGifSelect, gif} = this.props
+    const {gif} = this.props
+    // note: this is a very naive validation
+    if (!gif || typeof gif !== 'object') {
+      return (
+        <div className="gif-item"/>
+      )
+    }
+
     return (
-      <div className="gif-item"
-           onClick={() => {
-             onGifSelect(gif)
-           }}>
-        <img alt='' src={gif.images.downsized.url}/>
+      <div className="gif-item" onClick={this.selectGif}>
+        <img className='gif-img'
+             alt=''
+             src={gif.images.downsized.url}/>
       </div>
     )
   }
 }
+
 export default GifItem
+
+
