40c362565fc41a1e3dac8e117e4bd3532b5cbaef
marianna <marianna.etelson@nielsen.com>
2018-04-08 11:29:48 +0300

    preapre barnch for testing react components workshop part

--- src/gif-search-app/components/gifList/GifItem.js
+++ src/gif-search-app/components/gifList/GifItem.js
@@ -1,40 +1,21 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
 
 class GifItem extends Component {
 
-  static propTypes = {
-    gif: PropTypes.object.isRequired,
-    onGifSelect: PropTypes.func
-  }
-
-  selectGif = () => {
-    const {onGifSelect, gif} = this.props
-    if (onGifSelect) {
-      onGifSelect(gif)
-    }
-  }
 
   render() {
 
-    const {gif} = this.props
-    // note: this is a very naive validation
-    if (!gif || typeof gif !== 'object') {
-      return (
-        <div className="gif-item"/>
-      )
-    }
-
+    const {onGifSelect, gif} = this.props
     return (
-      <div className="gif-item" onClick={this.selectGif}>
-        <img className='gif-img'
-             alt=''
-             src={gif.images.downsized.url}/>
+      <div className="gif-item"
+           onClick={() => {
+             onGifSelect(gif)
+           }}>
+        <img alt='' src={gif.images.downsized.url}/>
       </div>
     )
   }
 }
-
 export default GifItem
 
 
